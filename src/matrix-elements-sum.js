9780cac2dc65435ede7b9431b34489f6bfb9466d
Roman Troshin <roman.troshin98@gmail.com>
2022-04-28 13:40:56 +0300

    feat: matrix-elements-sum solution

--- src/matrix-elements-sum.js
+++ src/matrix-elements-sum.js
@@ -16,10 +16,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+function getMatrixElementsSum(matrix) {
+  let sum = 0;
+  let sumFirstRow = matrix[0].reduce((acc, cur) => acc + cur, 0);
+
+  for (let i = 1; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i - 1][j] !== 0) {
+        sum += matrix[i][j];
+      };
+    };
+  };
+
+  return sumFirstRow + sum;
+};
 
 module.exports = {
   getMatrixElementsSum
